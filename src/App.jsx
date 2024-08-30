0efcccc8f6735c5cb9814e4e3544511cc611bc2f
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-08-30 22:51:23 +0530

    update header anavbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Layout className="mainLayout">
       <Header>
-        <AppHeader/>
+        <AppHeader/>  
       </Header>
       <Content>
         <AppContent />

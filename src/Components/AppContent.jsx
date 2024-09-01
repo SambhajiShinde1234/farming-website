f59311a67718dfb8a8b1d9fd204504f7a8bc8a86
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-09-01 21:55:42 +0530

    add gallery

--- src/Components/AppContent.jsx
+++ src/Components/AppContent.jsx
@@ -4,6 +4,7 @@ import About from '../Pages/About';
 import Products from '../Pages/Products';
 import WhyChooseUs from '../Pages/WhyChooseUs'; 
 import Testimonial from '../Pages/Testimonial';
+import Gallery from '../Pages/Gallery';
 import Contact from '../Pages/Contact';
 
 const AppContent = () => {
@@ -12,6 +13,7 @@ const AppContent = () => {
         <Hero />
         <Products />
         <About />
+        <Gallery />
         <WhyChooseUs />
         <Testimonial />
         {/* <Contact /> */}
---
+++ src/Pages/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div className='containerBg'>
+        <div className='container-fluid'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Gallery
